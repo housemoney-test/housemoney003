03260b444a981ad4879716f53ee4a3524f108dee
arimadaichi <arima.daichi@alhinc.jp>
2023-04-20 13:30:10 +0900

    first commit

---
+++ src/main/java/com/example/housemoney2/Housemoney2Application.java
@@ -0,0 +1,13 @@
+package com.example.housemoney2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Housemoney2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Housemoney2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/housemoney2/Housemoney2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housemoney2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Housemoney2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
