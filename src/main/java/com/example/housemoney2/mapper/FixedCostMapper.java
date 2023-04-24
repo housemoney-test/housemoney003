c1679973daa5933216e271e81d963941b34ea4b8
egutiryuuya <se.eguti0824@gmail.com>
2023-04-24 11:11:26 +0900

    a

--- src/main/java/com/example/housemoney2/mapper/FixedCostMapper.java
+++ src/main/java/com/example/housemoney2/mapper/FixedCostMapper.java
@@ -1,20 +1,20 @@
-package com.example.housemoney2.mapper;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.example.housemoney2.entity.FixedCost;
-
-@Mapper
-public interface FixedCostMapper {
-
-	List<FixedCost> findAll();
-
-	int create(FixedCost entity);
-	
-	int edit(FixedCost entity);
-
-	FixedCost findById(int id);
-    
-}
+//package com.example.housemoney2.mapper;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//import com.example.housemoney2.entity.FixedCost;
+//
+//@Mapper
+//public interface FixedCostMapper {
+//
+//	List<FixedCost> findAll();
+//
+//	int create(FixedCost entity);
+//	
+//	int edit(FixedCost entity);
+//
+//	FixedCost findById(int id);
+//    
+//}
